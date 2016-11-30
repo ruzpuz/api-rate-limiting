f2913ef3f25ee508c9ffc6da56559022cf719ff6
Vladimir Ruzic <vladimir.ruzic@bel-dev.rs>
2016-11-30 16:06:15 +0100

    ToDo - port cookie parser

--- rate-limiting/rate-limiting.route.js
+++ rate-limiting/rate-limiting.route.js
@@ -1,6 +1,12 @@
 (function() {
     'use strict';
 
+    function handleCall(req, res, next) {
+
+    }
+    function includeRoute(app, configuration) {
+
+    }
     module.exports = function (app, config) {
         app.use('/api/*', function (req, res, next) {
             require('./rate-limiting.controller').handleApiCall('12345', config, function (error) {
