8c48b3767d46541957af4ded87fd07e47d9c24b3
Vladimir Ružić <vladimir@ruzic.info>
2016-12-01 21:33:54 +0100

    preparing for release

--- index.js
+++ index.js
@@ -2,32 +2,13 @@
     'use strict';
 
     var rateLimitingService = require('./rate-limiting/rate-limiting.service.js'),
-        rateLimitingMiddleware = require('./rate-limiting/rate-limiting.middleware.js'),
-        testConfig = {
-        "calls" : 1,
-        "time" : 100,
-        "unit" : 'minutes',
-        "burst": 10,
-        "uniqueField" : {
-            "section" : 'header',
-            "name" : 'token'
-        },
-        "cookieParser" : {
-            "secret" : '',
-            "options" : ''
-        }
-    };
+        rateLimitingMiddleware = require('./rate-limiting/rate-limiting.middleware.js');
 
     function configure(configuration) {
         rateLimitingService.validate(configuration);
         return rateLimitingMiddleware.createMiddleware(configuration);
     }
 
-    function test() {
-        configure(testConfig);
-    }
-    test();
-    module.exports = {
-        "configure" :  configure
-    };
+    module.exports = configure;
+
 }());
