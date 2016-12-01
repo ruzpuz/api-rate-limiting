94011f901bf2a7175f32c123f5690bc2b58baee9
Vladimir Ružić <vladimir@ruzic.info>
2016-12-01 23:04:40 +0100

    scope handle

--- rate-limiting/rate-limiting.middleware.js
+++ rate-limiting/rate-limiting.middleware.js
@@ -5,22 +5,19 @@
 
 
     function createMiddleware(configuration) {
-
-        var uniqueField;
-
-        function  getUniqueField(req) {
-            if(configuration.uniqueField.section === 'ip') {
-                uniqueField = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-            } else if(configuration.uniqueField.section === 'header') {
-                uniqueField = req.headers[configuration.uniqueField.name];
-            } else {
-                require('../common/cookie-parsing.service').parse(req);
-                uniqueField = req.cookies[configuration.uniqueField.name];
-            }
-
-
-        }
         function handleCall(req, res, next) {
+            var uniqueField;
+
+            function  getUniqueField(req) {
+                if(configuration.uniqueField.section === 'ip') {
+                    uniqueField = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+                } else if(configuration.uniqueField.section === 'header') {
+                    uniqueField = req.headers[configuration.uniqueField.name];
+                } else {
+                    require('../common/cookie-parsing.service').parse(req);
+                    uniqueField = req.cookies[configuration.uniqueField.name];
+                }
+            }
             getUniqueField(req);
             rateLimitingController.handleApiCall(uniqueField, configuration, function (error) {
                 if(error) {
